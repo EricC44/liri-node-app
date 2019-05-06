a663c98eb0fd6a1adfd81cf42ac3e95bf7ec3597
ECarlson <ericcarlson4@outlook.com>
2019-05-05 21:44:49 -0700

    Need to figure out why my spotify keys aren't syncing up

--- keys.js
+++ keys.js
@@ -6,14 +6,14 @@ exports.spotify = {
     secret: process.env.SPOTIFY_SECRET
 };
 //This is the Bands in Town process.env call preparing to be exported
-exports.bandsInTown = {
-    id: process.env.BANDSINTOWN_ID
-}
+//exports.bandsInTown = {
+    //id: process.env.BANDSINTOWN_ID
+//}
 //This is the OMDB process.env call to prepare to be exported 
-exports.omdb = {
-    id: process.env.OMDB_ID
-}
+//exports.omdb = {
+    //id: process.env.OMDB_ID
+//}
 //This is the mapqu
-exports.mapquest = {
-    id: process.env.MAPQUEST_ID
-}
+//exports.mapquest = {
+    //id: process.env.MAPQUEST_ID
+//}

557c043b5d7a9015175af79b45f33a47adb9b4c9
ECarlson <ericcarlson4@outlook.com>
2019-05-08 19:38:18 -0700

    Completed the Spotify portion

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 //Require the Request
 var request = require("request");
 //Require file systems
-//const fs = require("fs");
+const fs = require("fs");
 //Rquire moment for Bands in Town information
 //const moment = require("moment");
 //Require the Spotify key page here
@@ -32,40 +32,48 @@ function userPrompt(userType, userQuery) {
     }
 }
 
-userCommand(userType, userQuery);
-
+userPrompt(userType, userQuery);
+// spotify
+//   .search({ type: 'track', query: 'All the Small Things' })
+//   .then(function(response) {
+//     console.log(response);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
 
 function spotifyThisSong() {
-    //If the query is not found or is undefinded, it will push "Breaking the Habit" by Linkin Park
+    
+    //If the query is not found or is undefinded, it will push "Ace of Base" by The Sign
+    console.log(`\n ~~~~~~~~~~~Finding ${userQuery}~~~~~~~~~~~~\n`)
     if(!userQuery) {
         userQuery = "the sign ace of base"
     }
-    console.log(`\n ~~~~~~~~~~~Finding ${userQuery}~~~~~~~~~~~~\n`)
+    
     //This is the format I want when I'm searching for a song
     spotify.search({
         //Type: is the song title, Query: Referes to the information regarding that query, and Limit: takes a maximum number of queries, in this case we only want one
         type: "track",
         query: userQuery,
         limit: 1
-    }), 
+    }, 
     //This function will either return an error or it will pull the data we are requesting
     function(error, data) {
         if(error) {
-            return console.log(`\nError: ${error}\n`);
+         console.log(`\nError: ${error}\n`);
+         return;
         }
-        //Data that was collected from the spotify api
-        var spotifyArry = data.tracks.items;
-        //A for loop that prints out the useful information of the information
-        for(i = 0; i < spotifyArry.length; i++) {
-            console.log("\n****************************************************\n")
-            console.log("I think I have found what you are looking for")
-            console.log(`\nArtist: ${data.tracks.items[i].album.artists[0].name}\n
-                         \nSong: ${data.tracks.items[i].name}\n
-                         \nAlbum: ${data.tracks.items[i].album.name}\n
-                         \nLink: ${data.tracks.items[i].external_urls.spotify}\n`)
-            console.log("\n****************************************************\n")
-        }
-    }
+
+        console.log("I think I know what you are looking for...")
+        //This returns all of the data around the song of choice
+        console.log("****************************************")
+        console.log(`\nArtist(s): ${data.tracks.items[0].artists[0].name}\n`);
+        console.log(`\nSong Title: ${data.tracks.items[0].name}\n`);
+        console.log(`\nAlbum: ${data.tracks.items[0].album.name}\n`);
+        console.log(`\nPreview: ${data.tracks.items[0].preview_url}\n`);
+        console.log("****************************************")
+        
+    });
 }
 function doThis() {
     //Use the file systems method to access the random.txt page
@@ -74,13 +82,14 @@ function doThis() {
             return console.log(`Error occured: ${error}`);
         }
         //Grab the data from the txt file and seperate the objects with the split method
-        var dataArry = data.split(",");
+        var dataArray = data.split(",");
         //Take the objects from the txt file and pass it through our userQuery parameters
-        userType = dataArry[0];
-        userQuery = dataArry[1];
+        userType = dataArray[0];
+        userQuery = dataArray[1];
+       
 
         //call the function with the new parameters
-        userCommand(userType, userQuery);
+        userPrompt(userType, userQuery);
     });
 
 

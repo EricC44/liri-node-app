ab2c000b5eb64ffda595c5c6b72d61d1b1adaa9c
ECarlson <ericcarlson4@outlook.com>
2019-05-09 18:24:03 -0700

    Trying to initialize my bands in town api key through my keys and env pages

--- keys.js
+++ keys.js
@@ -5,9 +5,9 @@ exports.spotify = {
     secret: process.env.SPOTIFY_SECRET
 };
 //This is the Bands in Town process.env call preparing to be exported
-//exports.bandsInTown = {
-    //id: process.env.BANDSINTOWN_ID
-//}
+exports.bandsInTown = {
+    id: process.env.BANDSINTOWN_ID
+}
 //This is the OMDB process.env call to prepare to be exported 
 //exports.omdb = {
     //id: process.env.OMDB_ID
